5fc65dbffbd035b00c1e8aa0d135b3038932781e
dev-miguel-resources <ingenieromiguelch@gmail.com>
2021-12-22 11:07:05 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+import { ContrasenaPipe } from './pipes/contrasena.pipe';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizadoPipe,
+    ContrasenaPipe,
+    DomseguroPipe
   ],
   imports: [
     BrowserModule
