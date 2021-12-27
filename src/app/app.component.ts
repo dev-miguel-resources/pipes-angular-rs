d4d8fdaa8e57c2854fcb4d036f757ff1e90bffd9
dev-miguel-resources <ingenieromiguelch@gmail.com>
2021-12-27 12:03:13 -0300

    working with new pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,23 @@ export class AppComponent {
   PI: number = Math.PI;
   a: number = 0.234;
   salario: number = 1234.5;
+  
+  persona: any = {
+    nombre: "Miguel",
+    apellido: "Fuentes",
+    edad: 29,
+    direccion: {
+      calle: "Sotomayor",
+      casa: "525",
+    },
+  }
 
+  valorDePromesa: any = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("Llegó la info!"), 3500)
+  })
+
+  fecha: Date = new Date();
+  nombre2: string = "jOrgE ANtoniO Perez domingueZ";
+  activar: boolean = true;
 
 }
--- src/app/pipes/capitalizado.pipe.ts
+++ src/app/pipes/capitalizado.pipe.ts
@@ -4,7 +4,21 @@ import { Pipe, PipeTransform } from '@angular/core'
   name: 'capitalizado',
 })
 export class CapitalizadoPipe implements PipeTransform {
-  transform() {
-    
+  transform(value: string, todas: boolean = true ): string {
+      value = value.toLowerCase();
+
+      let nombres = value.split(" ");
+
+      if (todas) {
+        for (let i in nombres) {
+          // la primera palabra del i y la primera letra de esa palabra 0
+          nombres[i] = nombres[i][0].toUpperCase() + nombres[i].substring(1);
+        }
+      } else {
+        // para la primera posición o palabra con su primera letra
+        nombres[0] = nombres[0][0].toUpperCase() + nombres[0].substring(1);
+      }
+
+      return nombres.join(" ");
   }
 }
